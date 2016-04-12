7d7db16206c9bb27fcf494159c484064f47926e5
mrpbueno <mrpbueno@gmail.com>
2016-04-12 19:36:54 -0300

    setMacAttribute

--- app/Mac.php
+++ app/Mac.php
@@ -8,4 +8,10 @@ class Mac extends Model
 {
     protected $table = 'mac';
     protected $fillable = array('mac','id_user','id_dev','ticket','nome_eq','ativo');
+    
+    /** Salva o campo mac com letra maiuscula */
+    public function setMacAttribute($value)
+	{       
+		$this->attributes['mac'] = strtoupper($value);
+	}
 }
